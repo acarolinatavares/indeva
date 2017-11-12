375c451b9fa7cfffd7a25ec7c45ca1d9875b80b1
atavares <atavares@id.uff.br>
2017-11-12 19:08:21 -0200

    Adicionando Rolify e Cancancan

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,5 @@
 class Owner < ApplicationRecord
+  rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :owners
+
+  root 'stores#index'
+
   resources :goal_date_employees
   resources :goal_dates
   resources :goals
   resources :employees
   resources :stores
-  devise_for :owners
 end
