59829ca121dc3a2e6d9512ddee81bda611ac7b44
atavares <atavares@id.uff.br>
2017-11-12 20:19:25 -0200

    Adicionando simple form e cocoon

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,7 @@
 class Store < ApplicationRecord
   resourcify
   belongs_to :owner
-  has_many :employees
+  has_many :employees, inverse_of: :store
+  accepts_nested_attributes_for :employees, reject_if: :all_blank, allow_destroy: true
+  validates :employees, presence: true
 end
