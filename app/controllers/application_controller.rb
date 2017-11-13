e3e5b37cee931bdbd408dd83ba205dac2d7fcc93
atavares <atavares@id.uff.br>
2017-11-12 23:25:44 -0200

    Permissionamento

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_owner!
+
+  def current_ability
+    @current_ability ||= Ability.new(current_owner)
+  end
 end
