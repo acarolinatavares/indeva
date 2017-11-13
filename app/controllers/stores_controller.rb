bb56d3fadf85ad5004b7742c4635324754f694f0
atavares <atavares@id.uff.br>
2017-11-12 22:59:46 -0200

    Melhorando layout das páginas da loja

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -19,7 +19,7 @@ class StoresController < ApplicationController
     @store = Store.new(store_params)
 
     if @store.save
-      flash[:notice] = 'Loja creada com sucesso.'
+      flash[:notice] = 'Loja criada com sucesso.'
       redirect_to @store
     else
       flash[:notice] = 'Erro ao cadastrar a loja. Verifique se todos os campos foram preenchidos, e se pelo menos um funcionário foi vinculado à loja.'
@@ -28,23 +28,19 @@ class StoresController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @store.update(store_params)
-        format.html { redirect_to @store, notice: 'Store was successfully updated.' }
-        format.json { render :show, status: :ok, location: @store }
-      else
-        format.html { render :edit }
-        format.json { render json: @store.errors, status: :unprocessable_entity }
-      end
+    if @store.update(store_params)
+      flash[:notice] = 'Loja atualizada com sucesso.'
+      redirect_to @store
+    else
+      flash[:notice] = 'Erro ao atualizar a loja. Verifique se todos os campos foram preenchidos, e se pelo menos um funcionário foi vinculado à loja.'
+      render :edit
     end
   end
 
   def destroy
     @store.destroy
-    respond_to do |format|
-      format.html { redirect_to stores_url, notice: 'Store was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Loja apagada.'
+    redirect_to stores_url
   end
 
   private
